32101d4f85fb63f170b69f19aee9ed3750b38abf
Joey3001 <josephnero111@gmail.com>
2021-01-08 09:16:08 -0800

    Add Engineer & Machine models.

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.Machines = new HashSet<EngineerMachine>(); 
+    }
+    public int EngineerId { get; set; }
+    public string Name { get; set;}
+    public ICollection<EngineerMachine> Machines { get; set; } 
+  }
+}
