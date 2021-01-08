101587fc52e00fc8ce1d8958f8f30d5f665d1e7d
Joey3001 <josephnero111@gmail.com>
2021-01-08 11:31:58 -0800

    Add Index & Create route for Machine controller.

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db; 
+    public MachinesController(FactoryContext db)
+    {
+      _db = db; 
+    }
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines
+        .Include(machine => machine.Machines)
+        .ThenInclude(join => join.Machine)
+        .ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View(); 
+    }
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    } 
+  }
+}
