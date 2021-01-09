853def76525a629d26a2edcdb3063ed41a5fba46
Joey3001 <josephnero111@gmail.com>
2021-01-08 16:25:17 -0800

    Update Splash Page to list all Engineers.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller 
   {
+    private readonly FactoryContext _db; 
+    public HomeController(FactoryContext db)
+    {
+      _db = db; 
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View(); 
+      List<Engineer> model = _db.Engineers
+        .Include(engineer => engineer.Machines)
+        .ThenInclude(join => join.Machine)
+        .ToList();
+      return View(model); 
     }
   }
 }
